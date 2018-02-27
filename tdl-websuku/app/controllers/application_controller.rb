834bc183794644ab90416740161c348175c4ff5a
fijimoto0895 <fujimotosiro@gmail.com>
2018-02-27 20:47:45 +0900

    home
    home.index

--- tdl-websuku/app/controllers/application_controller.rb
+++ tdl-websuku/app/controllers/application_controller.rb
@@ -1,16 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_cart
-
-  def current_cart
-  	if session[:cart_id]
-  		@cart = Cart.find(session[:cart_id])
-  	else
-  		@cart = Cart.create
-  		session[:cart_id] = @cart.id
-  		@cart
-  	end
-  end
-
-end
+  
