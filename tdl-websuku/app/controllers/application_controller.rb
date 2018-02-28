732cfe087f7204aa8872e2526d5fa276a5732b3d
TakaoOsada <try-osada@yahoo.ne.jp>
2018-02-28 18:14:26 +0900

    finished_cart!
    カート調整

--- tdl-websuku/app/controllers/application_controller.rb
+++ tdl-websuku/app/controllers/application_controller.rb
@@ -4,13 +4,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_cart
 
   def current_cart
-  	if session[:cart_id]
-  		@cart = Cart.find(session[:cart_id])
-  	else
-  		@cart = Cart.create(user_id: current_user.id)
-  		session[:cart_id] = @cart.id
+    if Cart.find_by(user_id: current_user.id)
+      @cart = Cart.find_by(user_id: current_user.id)
+    else
+      @cart = Cart.create(user_id: current_user.id)
       @cart
-  	end
+    end
   end
 
 end
